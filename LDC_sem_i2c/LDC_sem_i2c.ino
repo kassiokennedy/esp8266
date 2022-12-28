c2f5d50ca2200776db096594106bc4740d818f2a
kassiokennedy <kassio1206@hotmail.com>
2022-12-28 18:13:16 -0300

    esp

---
+++ LDC_sem_i2c/LDC_sem_i2c.ino
@@ -0,0 +1,36 @@
+//https://diyi0t.com/lcd-display-tutorial-for-arduino-and-esp8266/
+// https://dvtecnologia.com.br/imagens/HC1627-SYH-I2C-30_Eng%2CA.pdf - datasheet do display 
+
+#include <LiquidCrystal.h>
+
+#define PIN_WIRE_SDA (4)
+#define PIN_WIRE_SCL (5)
+
+static const uint8_t D0   = 16;
+static const uint8_t D1   = 5;
+static const uint8_t D2   = 4;
+static const uint8_t D3   = 0;
+static const uint8_t D4   = 2;
+static const uint8_t D5   = 14;
+static const uint8_t D6   = 12;
+static const uint8_t D7   = 13;
+static const uint8_t D8   = 15;
+static const uint8_t D9   = 3;
+
+int RS = D2, EN = D3, d4 = D5, d5 = D6, d6 = D7, d7 = D8;   
+LiquidCrystal lcd(RS, EN, d4, d5, d6, d7);
+
+void setup() {
+                         // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+                      // Print a message to the LCD.
+  lcd.print("hello, world!");
+}
+
+void loop() {
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print(millis() / 1000);
+}
